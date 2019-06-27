1b83f4c8d7a4b37f9ea8594b299c88d7c374505d
maliangwolf <maliang@credit-first.com.cn>
2019-06-27 14:42:09 +0800

    fix:添加获取getPropertie方法

---
+++ spring-cloud-client-config-demo/src/main/java/com/example/springcloudclientconfigdemo/controller/StudyController.java
@@ -0,0 +1,22 @@
+package com.example.springcloudclientconfigdemo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ml
+ * @title: StudyController
+ * @projectName spring-cloud-client-config-demo
+ * @description: TODO
+ * @date 2019/6/27 14:31
+ */
+@RestController
+public class StudyController {
+    @Value("${my.name}")
+    private String name;
+    @RequestMapping("/getPropertie")
+    public String getPropertie(){
+        return name;
+    }
+}
